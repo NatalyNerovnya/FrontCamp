a16f1e3946dfbd3ce8c6e1c3c98b372c4d6a638a
Nataly Nerovnya <natanerovnja@gmail.com>
2016-12-10 14:22:31 +0300

    Revert "Revert "add source repository""
    This reverts commit 1c240755274b0732f85c33b1bf0c4462742a0499.

--- 4_design-patterns/js/events.js
+++ 4_design-patterns/js/events.js
@@ -2,20 +2,12 @@ export let clickDropdown = id => document.getElementById(id).classList.toggle("s
 
 
 window.onload= function(){document.getElementsByClassName('dropbtn')[0].addEventListener('click',() => {
-	require.ensure(['./newsSources.js', './newSourcesDe.js'],() => {		
+	require.ensure(['./newsSources.js'],() => {		
 			var lang = document.querySelector('input[name="lang"]:checked').value;	
-			var sources;
 			home.clearContent("source-filter");
-			if(lang === "en"){	
-				let src = require("./newsSources.js");
-				sources = new src.newsSources();
-			} 
-			else if(lang === "de"){
-				let src = require("./newSourcesDe.js");
-				sources = new src.newSourcesDe();
-			}
-
-			sources.setSources();
+			let src = require("./newsSources.js");
+			let	sources = new src.newsSources();
+			sources.setSources(lang);
 			
 	});
 
